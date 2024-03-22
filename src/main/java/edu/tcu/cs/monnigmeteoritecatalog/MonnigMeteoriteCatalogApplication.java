77f6e39b89ac8a6ee16892e891e3d2cc8c92f612
RosShelashskyi <ross.shelashskyi@tcu.edu>
2024-03-22 09:55:31 -0500

    Empty project created

---
+++ src/main/java/edu/tcu/cs/monnigmeteoritecatalog/MonnigMeteoriteCatalogApplication.java
@@ -0,0 +1,13 @@
+package edu.tcu.cs.monnigmeteoritecatalog;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MonnigMeteoriteCatalogApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MonnigMeteoriteCatalogApplication.class, args);
+    }
+
+}
---
+++ src/test/java/edu/tcu/cs/monnigmeteoritecatalog/MonnigMeteoriteCatalogApplicationTests.java
@@ -0,0 +1,13 @@
+package edu.tcu.cs.monnigmeteoritecatalog;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MonnigMeteoriteCatalogApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
