44fae21660b3566e6e9dcd08c9baf492845c98ff
Ros Shelashskyi <ross.shelashskyi@tcu.edu>
2024-04-04 17:29:20 -0500

    attributes and constructor

--- src/main/java/edu/tcu/cs/monnigmeteoritecatalog/sample/SampleService.java
+++ src/main/java/edu/tcu/cs/monnigmeteoritecatalog/sample/SampleService.java
@@ -1,5 +1,24 @@
 package edu.tcu.cs.monnigmeteoritecatalog.sample;
 
-public class SampleService {
+import edu.tcu.cs.monnigmeteoritecatalog.utils.IdWorker;
+import jakarta.transaction.Transactional;
+import org.springframework.stereotype.Service;
 
+@Service
+@Transactional
+public class SampleService {
+    private final SampleRepository sampleRepository;
+
+    private final IdWorker idWorker;
+
+    public SampleService(SampleRepository sampleRepository, IdWorker idWorker) {
+        this.sampleRepository = sampleRepository;
+        this.idWorker = idWorker;
+    }
+
+    //function for internal use for finding samples by internal ids
+//    public Sample findById(String sampleId){
+//        return this.sampleRepository.findById(sampleId)
+//                .orElseThrow(() -> )
+//    }
 }
