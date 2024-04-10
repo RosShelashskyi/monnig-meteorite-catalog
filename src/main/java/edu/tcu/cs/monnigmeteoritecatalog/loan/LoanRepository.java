209bc3b3bc008614ab5fc1d4685a7c3372bdaa9b
RosShelashskyi <ross.shelashskyi@tcu.edu>
2024-04-10 10:05:19 -0500

    Loan Repository done

--- src/main/java/edu/tcu/cs/monnigmeteoritecatalog/loan/LoanRepository.java
+++ src/main/java/edu/tcu/cs/monnigmeteoritecatalog/loan/LoanRepository.java
@@ -1,8 +1,7 @@
 package edu.tcu.cs.monnigmeteoritecatalog.loan;
 
-import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface LoanRepository extends JpaRepository<Loan, String>{
+public class LoanRepository {
 }
