10605a00f2243043a2a1afacc7d01b49b05a8c7b
RosShelashskyi <ross.shelashskyi@tcu.edu>
2024-04-10 20:15:48 -0500

    LoanService implemented

---
+++ src/main/java/edu/tcu/cs/monnigmeteoritecatalog/loan/LoanController.java
@@ -0,0 +1,10 @@
+package edu.tcu.cs.monnigmeteoritecatalog.loan;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/loan")
+public class LoanController {
+    
+}
