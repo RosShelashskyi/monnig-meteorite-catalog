66e4aeae017dc9688167c0a05c9422ac4bcc48d1
RosShelashskyi <ross.shelashskyi@tcu.edu>
2024-04-10 20:22:36 -0500

    Dto and converters for Loan implemented

---
+++ src/main/java/edu/tcu/cs/monnigmeteoritecatalog/loan/converter/LoanToLoanDtoConverter.java
@@ -0,0 +1,30 @@
+package edu.tcu.cs.monnigmeteoritecatalog.loan.converter;
+
+import edu.tcu.cs.monnigmeteoritecatalog.loan.Loan;
+import edu.tcu.cs.monnigmeteoritecatalog.loan.dto.LoanDto;
+import org.springframework.core.convert.converter.Converter;
+import org.springframework.stereotype.Component;
+
+import java.lang.annotation.Annotation;
+
+@Component
+public class LoanToLoanDtoConverter implements Converter<Loan, LoanDto> {
+    public LoanToLoanDtoConverter() {
+    }
+
+    @Override
+    public LoanDto convert(Loan source){
+        LoanDto loanDto = new LoanDto(source.getLoan_ID(),
+                                        source.getSamples_on_loan(),
+                                        source.getLoanee_name(),
+                                        source.getLoanee_email(),
+                                        source.getLoanee_institution(),
+                                        source.getLoanee_address(),
+                                        source.getLoan_start_date(),
+                                        source.getLoan_due_date(),
+                                        source.getLoan_notes()
+                                        );
+        return loanDto;
+    }
+
+}
