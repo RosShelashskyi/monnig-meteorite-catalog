6355006a2a6bd08bd84630abe8f35c5ea1bf8a62
Ros Shelashskyi <ross.shelashskyi@tcu.edu>
2024-04-13 21:00:36 -0500

    Fixed samples on loan in Loans

--- src/main/java/edu/tcu/cs/monnigmeteoritecatalog/loan/Loan.java
+++ src/main/java/edu/tcu/cs/monnigmeteoritecatalog/loan/Loan.java
@@ -1,6 +1,7 @@
 package edu.tcu.cs.monnigmeteoritecatalog.loan;
 
 import edu.tcu.cs.monnigmeteoritecatalog.sample.Sample;
+import jakarta.persistence.CascadeType;
 import jakarta.persistence.Entity;
 import jakarta.persistence.Id;
 import jakarta.persistence.OneToMany;
@@ -15,7 +16,7 @@ public class Loan implements Serializable {
     @Id
     private String loan_ID;
 
-    @OneToMany
+    @OneToMany(cascade = {CascadeType.PERSIST, CascadeType.MERGE})
     private List<Sample> samples_on_loan;
 
     private String loanee_name;
--- src/main/java/edu/tcu/cs/monnigmeteoritecatalog/system/DBDataInitializer.java
+++ src/main/java/edu/tcu/cs/monnigmeteoritecatalog/system/DBDataInitializer.java
@@ -1,20 +1,27 @@
 package edu.tcu.cs.monnigmeteoritecatalog.system;
 
+import edu.tcu.cs.monnigmeteoritecatalog.loan.Loan;
+import edu.tcu.cs.monnigmeteoritecatalog.loan.LoanRepository;
 import edu.tcu.cs.monnigmeteoritecatalog.sample.Sample;
 import edu.tcu.cs.monnigmeteoritecatalog.sample.SampleRepository;
 import edu.tcu.cs.monnigmeteoritecatalog.utils.IdWorker;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.stereotype.Component;
 
+import java.util.Arrays;
+
 @Component
 public class DBDataInitializer implements CommandLineRunner {
 
     private final SampleRepository sampleRepository;
 
+    private final LoanRepository loanRepository;
+
     private final IdWorker idWorker;
 
-    public DBDataInitializer(SampleRepository sampleRepository, IdWorker idWorker) {
+    public DBDataInitializer(SampleRepository sampleRepository, LoanRepository loanRepository, IdWorker idWorker) {
         this.sampleRepository = sampleRepository;
+        this.loanRepository = loanRepository;
         this.idWorker = idWorker;
     }
 
@@ -42,5 +49,30 @@ public class DBDataInitializer implements CommandLineRunner {
 
         sampleRepository.save(s1);
         sampleRepository.save(s2);
+
+        Loan l1 = new Loan();
+        l1.setLoan_ID(idWorker.nextId() + "");
+        l1.setLoanee_name("John Doe");
+        l1.setLoanee_email("johndoe@example.com");
+        l1.setLoanee_institution("Meteorite Research Institute");
+        l1.setLoanee_address("123 Meteorite Street, Cityville, USA");
+        l1.setLoan_start_date("2024-04-01 10:00:00");
+        l1.setLoan_due_date("2024-04-15 10:00:00");
+        l1.setLoan_notes("Fragile meteorite, handle with care.");
+        l1.setSamples_on_loan(Arrays.asList(s1));
+
+        Loan l2 = new Loan();
+        l2.setLoan_ID(idWorker.nextId() + "");
+        l2.setLoanee_name("Jane Smith");
+        l2.setLoanee_email("janesmith@example.com");
+        l2.setLoanee_institution("Space Exploration Society");
+        l2.setLoanee_address("456 Galaxy Avenue, Star City, Canada");
+        l2.setLoan_start_date("2024-03-20 09:30:00");
+        l2.setLoan_due_date("2024-04-10 09:30:00");
+        l2.setLoan_notes("Rare meteorite, research project.");
+        l2.setSamples_on_loan(Arrays.asList(s2));
+
+        loanRepository.save(l1);
+        loanRepository.save(l2);
     }
 }
